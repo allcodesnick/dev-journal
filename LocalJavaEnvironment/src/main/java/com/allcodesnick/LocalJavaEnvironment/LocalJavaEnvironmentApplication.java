a7473dd4e7bd211e805cd6e50e063766e331eabc
Nick Felix <allcodesnick@gmail.com>
2023-04-26 10:36:48 -0400

    init

---
+++ LocalJavaEnvironment/src/main/java/com/allcodesnick/LocalJavaEnvironment/LocalJavaEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.allcodesnick.LocalJavaEnvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalJavaEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalJavaEnvironmentApplication.class, args);
+	}
+
+}
---
+++ LocalJavaEnvironment/src/test/java/com/allcodesnick/LocalJavaEnvironment/LocalJavaEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allcodesnick.LocalJavaEnvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalJavaEnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
