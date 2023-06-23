9ca86f40b701138e002eca0e93bcb257914a4b44
Nick Felix <allcodesnick@gmail.com>
2023-06-23 16:30:10 -0400

    Changes to current nba-api script for sports betting

--- scripts/nba-api.js
+++ scripts/nba-api.js
@@ -1,42 +1,102 @@
-/** Custom Query Parameters Class Definition
- * Class Name: QueryParameters
- * Description:
- *  This class is used to define custom query parameters. It provides methods for custom query parameters.
- * 
- * */
 const axios = require('axios');
 
-const player = {
-  id: 0,
-  first_name: '',
-  height_feet: null,
-  height_inches: null,
-  last_name: '',
-  position: '',
-  team: [team],
-  weight_pounds: null
-}
+const urls = [
+  'https://www.balldontlie.io/api/v1/players',
+  'https://www.balldontlie.io/api/v1/teams',
+  'https://www.balldontlie.io/api/v1/games',
+  'https://www.balldontlie.io/api/v1/stats',
+  'https://www.balldontlie.io/api/v1/season_averages'
+];
 
-const team = {
-  id: 0,
-  abbreviation: '',
-  city: '',
-  conference: '',
-  division: '',
-  full_name: '',
-  name: ''
-}
 
 const options = {
     method: 'GET',
-    url: 'https://www.balldontlie.io/api/v1/teams'
+    url: urls[1]
   };
 
+const player_request = {
+  method: 'GET',
+  url: urls[0],
+  parms: [
+    {
+      player_id: 0,
+      page: 0,
+      per_page: 0,
+      search: ''
+    }
+  ]
+};  
+const teams_request = {
+  method: 'GET',
+  url: urls[1],
+  parms: [
+    {
+      team_id: 0,
+      page: 0,
+      per_page: 0
+    }
+  ]
+};
 
-// Asyn function to send request
+const games_request = {
+method: 'GET',
+url: urls[2],
+parms: [
+  {
+    page: 0,
+    per_page: 0,
+    dates: 'YYYY-MM-DD',
+    seasons: [],
+    team_ids: [],
+    postseason: false,
+    start_date: 'YYYY-MM-DD',
+    end_date: 'YYYY-MM-DD'
+  }
+]
+};
+
+const stats_request = {
+  method: 'GET',
+  url: urls[3],
+  parms: [
+    {
+      page: 0,
+      per_page: 0,
+      dates: 'YYYY-MM-DD',
+      seasons: [],
+      player_ids: [],
+      game_ids: [],
+      postseason: false,
+      start_date: 'YYYY-MM-DD',
+      end_date: 'YYYY-MM-DD'
+    }
+  ]
+};
+
+const season_request = {
+method: 'GET',
+url: urls[4],
+parms: [
+  {
+    season: '',
+    player_ids: []
+  }
+]
+}; 
+
+/**
+ * 1. Create custom request options for each url with each custom parameters
+ * 2. Create custom async request options for each url
+ * 3. Retreive response and save to Object
+ * 
+ * 
+ *  */ 
+
+
+// TEST - Asyn function to send request
 async function asyncCall() {
     try {
-        const response = await axios.request(options);
+        const response = await axios.request(player_request);
         console.log(response.data);
     } catch (error) {
         console.error(error);
