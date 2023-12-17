76c28bc1fefefe2f7bcf51870436235936be2b13
Nick Felix <allcodesnick@gmail.com>
2023-12-17 14:44:30 -0500

    local changes to files

--- LocalJavaEnvironment/src/main/java/com/allcodesnick/LocalJavaEnvironment/automatedscripts/ResumeBuilder.java
+++ LocalJavaEnvironment/src/main/java/com/allcodesnick/LocalJavaEnvironment/automatedscripts/ResumeBuilder.java
@@ -1,4 +1,7 @@
 package com.allcodesnick.LocalJavaEnvironment.automatedscripts;
 
 public class ResumeBuilder {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
