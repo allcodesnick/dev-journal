2b931b8b8a0cec31a7bd871fdfe7cd7ef844c49a
Nick Felix <allcodesnick@gmail.com>
2023-12-20 21:52:03 -0500

    login registration setup files

--- LocalJavaEnvironment/src/main/java/com/allcodesnick/den/skatemaddness/web/controller/api/HomeController.java
+++ LocalJavaEnvironment/src/main/java/com/allcodesnick/den/skatemaddness/web/controller/api/HomeController.java
@@ -1,7 +1,6 @@
 package com.allcodesnick.den.skatemaddness.web.controller.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
