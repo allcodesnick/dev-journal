b33af507a07cca7cca29f4c80cd5e6398dacf0ff
Nick Felix <allcodesnick@gmail.com>
2024-01-30 13:52:04 -0500

    java environment project structure

--- LocalJavaEnvironment/src/main/java/com/allcodesnick/den/exception/ResourceNotFoundException.java
+++ LocalJavaEnvironment/src/main/java/com/allcodesnick/den/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.allcodesnick.den.project1.helper.exception;
+package com.allcodesnick.den.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- LocalJavaEnvironment/src/main/java/com/allcodesnick/den/project1/helper/ResumeBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.allcodesnick.den.project1.helper;
-
-public class ResumeBuilder {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
- 
